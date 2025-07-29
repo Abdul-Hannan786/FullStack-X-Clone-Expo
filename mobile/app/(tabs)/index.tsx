d803e264464e0c6fc5eda819739d0afc0f061c94
Abdul Hannan <abdulhannanhere@gmail.com>
2025-07-30 02:48:55 +0500

    working on home page

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -5,12 +5,13 @@ import SignOutButton from "@/components/SignOutButton";
 import { useUserSync } from "@/hooks/useUserSync";
 import { Ionicons } from "@expo/vector-icons";
 import PostComposer from "@/components/PostComposer";
+import PostList from "@/components/PostList";
 
 const HomeScreen = () => {
   useUserSync();
 
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView className="flex-1 bg-white">
       <View className="flex-row justify-between items-center px-3 py-3">
         <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
         <Text className="font-bold text-xl text-gray-900">Home</Text>
@@ -23,6 +24,7 @@ const HomeScreen = () => {
         contentContainerStyle={{ paddingBottom: 80 }}
       >
         <PostComposer />
+        <PostList />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ mobile/components/PostCard.tsx
@@ -0,0 +1,77 @@
+import { View, Text, Alert, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { Post, User } from "@/types";
+import { formatDate } from "@/utils/formatter";
+import { Feather } from "@expo/vector-icons";
+
+interface PostCardProps {
+  post: Post;
+  onLike: (postId: string) => void;
+  onDelete: (postId: string) => void;
+  isLiked?: boolean;
+  currentUser: User;
+}
+
+const PostCard = ({
+  currentUser,
+  onDelete,
+  onLike,
+  isLiked,
+  post,
+}: PostCardProps) => {
+  const isOwnPost = post.user._id === currentUser._id;
+  const handleDelete = () => {
+    Alert.alert("Delete Post", "Are you sure you want to delete this post?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: () => onDelete(post._id),
+      },
+    ]);
+  };
+
+  return (
+    <View className="border-b border-gray-100 bg-white">
+      <View className="flex-row p-4">
+        <Image
+          source={{ uri: post.user.profilePicture || "" }}
+          className="w-12 h-12 rounded-full mr-3"
+        />
+        <View className="flex-1">
+          <View className="flex-row items-center justify-between mb-1">
+            <View className="flex-row items-center">
+              <Text className="font-bold text-gray-900 mr-1">
+                {post.user.firstName} {post.user.lastName}
+              </Text>
+              <Text className="text-gray-500 ml-1 text-sm">
+                @{post.user.username} · {formatDate(post.createdAt)}
+              </Text>
+            </View>
+            {isOwnPost && (
+              <TouchableOpacity onPress={handleDelete}>
+                <Feather name="trash" size={20} color="#657786" />
+              </TouchableOpacity>
+            )}
+          </View>
+
+          {post.content && (
+            <Text className="text-gray-900 text-base leading-5 mb-3">
+              {post.content}
+            </Text>
+          )}
+
+          {/* {post.image && (
+            <Image
+              source={{ uri: post.image }}
+              className="w-full h-48 rounded-2xl mb-3"
+              resizeMode="cover"
+            />
+          )} */}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default PostCard;
